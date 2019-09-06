79381fcf6f4a4e76785a3d99f379846761618ef8
Alexander Salois <asalois.scholar@gmail.com>
2019-09-05 18:42:18 -0600

    added basic function to stat

--- Project 1/python/stat_analysis.py
+++ Project 1/python/stat_analysis.py
@@ -1,1 +1,15 @@
 """ Functions for performing statistical analysis of our data and results """
+# make a function that checks from an orginal data set
+def difference(class_o, class_m):
+    if len(class_o) == len(class_m):
+        diff = []
+        for i in range(len(class_o)):
+            diff.append(class_o[i] - class_m[i])
+        print(diff)
+    else:
+        print("somethings wrong")
+
+x = [100, 10, 50]
+y = [100, 5, 25]
+difference(x, y)
+
