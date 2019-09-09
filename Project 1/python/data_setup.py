4d5c5b17c9ec99c261e48c6dc57a3dc03cc2f2a8
zan.montieth@gmail.com <Danceballet1CS?>
2019-09-09 11:36:35 -0600

    adding choices to data_setup.py

--- Project 1/python/data_setup.py
+++ Project 1/python/data_setup.py
@@ -1,16 +1,15 @@
 import os
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, '../Datasets/breast-cancer-wisconsin.data')
+filepath = os.path.join(dirname, '../Datasets/')
 
 """ This is for reading and preparing data for runs in through our ML algorithm """
 arr = []
-# dataFolder = "C:/Users/cache/Documents/SchoolFall2019/CSCI447_MachineLearning/Project1/Project 1/python/"                                                                        #hardcoded this because its way easier 
-# print("pls choose which file you would like\n 1: for Breast Cancer Data\n 2: for Glass Data\n 3: for Iris Data\n 4: for Soybean Data\n 5: for Vote Data ")
-# choice = input("you're choice: ")
-# files = ["breast-cancer-wisconsin.data","glass.data","iris.data","soybean-small.data","house-votes-84.data"]
+print("pls choose which file you would like\n 1: for Breast Cancer Data\n 2: for Glass Data\n 3: for Iris Data\n 4: for Soybean Data\n 5: for Vote Data ")
+choice = input("you're choice: ")
+files = ["breast-cancer-wisconsin.data","glass.data","iris.data","soybean-small.data","house-votes-84.data"]
 
 
-# fileToOpen = dataFolder + files[int(choice)-1]
+fileToOpen = filepath + files[int(choice)-1]
 
 fileIn = open (filename,"r")
 for line in fileIn.readlines():
