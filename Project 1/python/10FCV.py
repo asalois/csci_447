7a5a2bcdcf69b45a36a438bef153f3d2845bef36
Super-Original-Username <aswordlessninja@gmail.com>
2019-09-09 12:14:22 -0600

    added code that may or may not be broken. We'll see

--- Project 1/python/10FCV.py
+++ Project 1/python/10FCV.py
@@ -1,15 +1,20 @@
 """ This is for performing 10-fold crossvalidation of the ML algorithm """
 import random
+import data_setup
+import copy
 
-
+''' We don't need a function to do just this, but ¯\_₍ ツ ₎_/¯ '''
 def randomizer(samples):
+    return random.shuffle(samples)
 
 
+'''
+This function splits the dataset array into 10 roughly equal pieces to be
+used as test sets in 10fold cross validation
 
-''' This function splits the dataset array into 10 roughly equal pieces to be
-used as test sets in 10fold cross validation '''
-
-'''how to do this step was aquired form https://stackoverflow.com/questions/2130016/splitting-a-list-into-n-parts-of-approximately-equal-length'''
+The general concept for this function was aquired from
+https://stackoverflow.com/questions/2130016/splitting-a-list-into-n-parts-of-approximately-equal-length
+'''
 def splitter(samples):
     OG = samples
     shuf_samples = random.shuffle(samples)
@@ -18,4 +23,23 @@ def splitter(samples):
     start = 0
     while start < len(shuf_samples)
         split_array.append(seq[first:(first + average_len)])
-        
+        start += average_len
+
+    return split_array
+
+
+'''
+This also doesn't need its own function, but we're lazy people
+Calls the data read function from data_setup.py
+'''
+def get_list(input):
+    base_data = data_setup.readInComp(input)
+    return base_data
+
+
+def work_it():
+    og_data = get_list(1)
+    new_data = copy.copy(og_data)
+    new_data = randomizer(new_data)
+    new data = splitter(new_data)
+    print(new_data)
