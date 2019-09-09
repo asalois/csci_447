4382921c533df374bdc1043996478ee28e338c3c
Super-Original-Username <aswordlessninja@gmail.com>
2019-09-09 13:01:27 -0600

    added gitignores
    my code is broken. Big surprise

--- Project 1/python/10FCV.py
+++ Project 1/python/10FCV.py
@@ -1,9 +1,11 @@
 """ This is for performing 10-fold crossvalidation of the ML algorithm """
+# This file brought to you by Ethan
+
 import random
 import data_setup
 import copy
 
-''' We don't need a function to do just this, but ¯\_₍ ツ ₎_/¯ '''
+''' We don't need a function to do just this, but we added one anyway '''
 def randomizer(samples):
     return random.shuffle(samples)
 
@@ -21,7 +23,7 @@ def splitter(samples):
     average_len = len(samples) / 10
     split_array = []
     start = 0
-    while start < len(shuf_samples)
+    while start < len(shuf_samples):
         split_array.append(seq[first:(first + average_len)])
         start += average_len
 
@@ -33,13 +35,19 @@ This also doesn't need its own function, but we're lazy people
 Calls the data read function from data_setup.py
 '''
 def get_list(input):
-    base_data = data_setup.readInComp(input)
+    base_data = data_setup.readInCom(input)
     return base_data
+
+
+def list_printer(ls):
+    for i in range(len(ls)):
+        for j in range(len(ls[i])):
+            print(ls[i][j])
 
 
 def work_it():
     og_data = get_list(1)
     new_data = copy.copy(og_data)
     new_data = randomizer(new_data)
-    new data = splitter(new_data)
-    print(new_data)
+    new_data = splitter(new_data)
+    list_printer(new_data)
