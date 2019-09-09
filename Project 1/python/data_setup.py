524a8bcfb7d66284e14b3d8aa41f1ee2c3b24358
zan.montieth@gmail.com <Danceballet1CS?>
2019-09-09 12:01:49 -0600

    turned the file read in into a function

--- Project 1/python/data_setup.py
+++ Project 1/python/data_setup.py
@@ -1,34 +1,43 @@
+# """ This is for reading and preparing data for runs in through our ML algorithm """
 import os
 dirname = os.path.dirname(__file__)
 filepath = os.path.join(dirname, '../Datasets/')
 
-""" This is for reading and preparing data for runs in through our ML algorithm """
-arr = []
-print("pls choose which file you would like\n 1: for Breast Cancer Data\n 2: for Glass Data\n 3: for Iris Data\n 4: for Soybean Data\n 5: for Vote Data ")
-choice = input("you're choice: ")
-files = ["breast-cancer-wisconsin.data","glass.data","iris.data","soybean-small.data","house-votes-84.data"]
+
+def readInCom(input):
+    arr = []
+
+    files = ["breast-cancer-wisconsin.data","glass.data","iris.data","soybean-small.data","house-votes-84.data"]
+
+    fileToOpen = filepath + files[int(choice)-1]
+
+    fileIn = open (fileToOpen,"r")
+    for line in fileIn.readlines():
+        if line != "":
+            arr.append([])
+            for z in line.split(","):
+                z = z.rstrip()
+                if z.isdigit():
+                    arr[-1].append(int(z.rstrip()))
+                else:
+                    try:
+                        arr[-1].append(float(z))
+                    except ValueError:
+                        if z != '':
+                            arr[-1].append(z)
+    arr = [z for z in arr if z != []]                           # removes last list in the array in case input has new line at the end 
 
 
-fileToOpen = filepath + files[int(choice)-1]
+    for z in range(len(arr)):
+        print(arr[z])
 
-fileIn = open (fileToOpen,"r")
-for line in fileIn.readlines():
-    if line != "":
-        arr.append([])
-        for z in line.split(","):
-            z = z.rstrip()
-            if z.isdigit():
-                arr[-1].append(int(z.rstrip()))
-            else:
-                try:
-                    arr[-1].append(float(z))
-                except ValueError:
-                    if z != '':
-                        arr[-1].append(z)
-arr = [z for z in arr if z != []]                           # removes last list in the array in case input has new line at the end 
+    print("all Done")
 
 
-for z in range(len(arr)):
-    print(arr[z])
+def readInPerson():
+    print("pls choose which file you would like\n 1: for Breast Cancer Data\n 2: for Glass Data\n 3: for Iris Data\n 4: for Soybean Data\n 5: for Vote Data ")
 
-print("all Done")
+    readInCom(input("your choice: "))
+
+
+readInPerson()
