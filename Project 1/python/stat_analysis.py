d9eb2e1e27c18cf361a4ee7e267e82a7fcbbe4a3
Alexander Salois <asalois.scholar@gmail.com>
2019-09-11 14:53:58 -0600

    Made a Binary Confusion Matrix

--- Project 1/python/stat_analysis.py
+++ Project 1/python/stat_analysis.py
@@ -1,15 +1,42 @@
 """ Functions for performing statistical analysis of our data and results """
 # make a function that checks from an orginal data set
-def difference(class_o, class_m):
-    if len(class_o) == len(class_m):
+
+# clalculates the error base upon a confusion matrix
+def errorCalc(tp,fp,fn,tn):# True+, False+, False-, True-
+    err =  (fp + fn) / (tp + fp + fn + tp)
+    precision = tp / (tp + fp)
+    recall = tp / ( tp + fn)
+    error = [err, precision, recall]
+    return error
+
+
+# simple binary confusion Matrix builder
+def makeConfMatrix(classO, classM):
+    conf = [0,0,0,0] # [True+ False+ False- True-]
+    if classO == classM:
+        if classO == 1:
+            conf[0] += 1
+        else:
+            conf[3] += 1
+    else:
+        if classO == 1:
+            conf[2] += 1
+        else:
+            conf[1] += 1
+    return conf
+
+
+def difference(classO, classM):
+    if len(classO) == len(classM):
         diff = []
-        for i in range(len(class_o)):
-            diff.append(class_o[i] - class_m[i])
+        for i in range(len(classO)):
+            diff.append(classO[i] - classM[i])
         print(diff)
     else:
         print("somethings wrong")
 
-x = [100, 10, 50]
-y = [100, 5, 25]
-difference(x, y)
+
+x = [0, 0, 0, 0, 0, 0]
+y = [0, 1, 0, 1, 0, 1]
+print(makeConfMatrix(1, 1))
 
