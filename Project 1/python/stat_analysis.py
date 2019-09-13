48a1b12b8506d869866a76ac4037d944fc26baf9
Super-Original-Username <aswordlessninja@gmail.com>
2019-09-13 09:48:30 -0600

    we need to start committing our stuff. pls

--- Project 1/python/stat_analysis.py
+++ Project 1/python/stat_analysis.py
@@ -5,7 +5,7 @@ import data_setup
 import copy
 import random
 # calculates the error base upon a confusion matrix
-def calcError(filled):
+def calcError(filled, num_classes):
     error =  numpy.full((3,len(filled)),0.0)
     for i in range(len(filled) -1):
         fp = filled[i][-1] -  filled[i][i]
