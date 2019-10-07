cb4bacff70742295a854687d235e32252c65280f
zan.montieth@gmail.com <Danceballet1CS?>
2019-10-06 22:30:45 -0600

    finished most Cmeans

--- project_2/python/actual code/CMEANS.py
+++ project_2/python/actual code/CMEANS.py
@@ -4,12 +4,12 @@ import math
 import EKNN
 
 class c_means(EKNN.edited_knn):
-    def __init__(self, kNum, numCentroids, dataMap):
-        self.kNum = kNum
+    def __init__(self, k, numCentroids, dataMap):
+        self.kNum = k
+        EKNN.edited_knn(k, dataMap)
         self.numC = numCentroids
         self.data_Map = dataMap
         self.c_means_map = ''
-
     
     def placeInitialC (self, numC, indexes, dataMap):
         point_map = []
@@ -24,7 +24,35 @@ class c_means(EKNN.edited_knn):
             while not unique:
                 index = rd.randint(0,len(self.data_Map))
                 if index not in l:
-                    l[x] = index
+                    l.append(index)
                     unique = True
         return l
 
+    def calcCentoids(self):
+        children = []
+        for k in self.data_Map:
+            pointBelongsToo = self.data_point()
+            shortestDist = 9223372036854775807
+            for i in self.c_means_map:
+                if(self.euclidian(k.data,i.data) < shortestDist):
+                    pointBelongsToo = i
+            children.append([k, pointBelongsToo])
+
+        for cluster in self.c_means_map:    
+            points = []
+            for point in children:
+                if(point[1] == cluster):
+                    points.append(point[0])
+            ind = self.c_means_map.index(cluster)
+            self.c_means_map[ind].data = self.findAverage(points)
+
+    def findAverage(self,points):
+        newData = []
+        for x in range (0,len(points[0].data)):
+            newData.append(0)
+            avg = []
+            for point in points:
+                avg.append(point.data[0])
+            value = sum(avg) /len(avg)
+            newData[x] = value
+        return newData
